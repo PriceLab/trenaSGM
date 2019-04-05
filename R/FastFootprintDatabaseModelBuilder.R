72a4621a90aa0f8be30736fd08a1f1e5e7f06586
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-04-05 16:06:08 -0700

    browser() removed

--- R/FastFootprintDatabaseModelBuilder.R
+++ R/FastFootprintDatabaseModelBuilder.R
@@ -226,8 +226,6 @@ setMethod('staged.fast.build', 'FastFootprintDatabaseModelBuilder',
            printf("--- mapping %d motifs to genes", length(motifs.to.map))
            print(system.time(tbl.motif2tf <- motifToGene(MotifDb, unique(tbl.fp$name), c("MotifDb", "TFClass"))))
            tf.candidates <- sort(unique(tbl.motif2tf$geneSymbol))
-           browser()
-           xyz <- "ENSG coming up"
            if(grepl("^ENSG", obj@targetGene)){  # rather crude hack for now.  TODO: refactor ensg/geneSymol navigation
               suppressMessages(
                  tf.candidates.ensg <- select(org.Hs.eg.db, keys=tf.candidates, keytype="SYMBOL",
